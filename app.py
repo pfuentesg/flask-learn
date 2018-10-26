d90ced6bed302317d20f40bafb43ffcd0403bf28
Pablo.Fuentes <pablo.fuentes@intelygez.com>
2018-10-26 20:47:59 +0200

    include controllers

--- app.py
+++ app.py
@@ -2,9 +2,35 @@ from flask import Flask
 
 app = Flask(__name__)
 
+stores = [{
+    'name': 'My Store',
+    'items': [{'name':'my item', 'price': 15.99 }]
+}]
+
 @app.route('/')
 def home():
     return "holi"
+
+
+@app.route('/store' , methods=['POST'])
+def create_store():
+    pass
+
+@app.route('/store/<string:name>')
+def get_store(name):
+    pass
+
+@app.route('/store')
+def get_stores():
+    pass
+
+@app.route('/store/<string:name>/item' , methods=['POST'])
+def create_item_in_store(name): 
+    pass
+
+@app.route('/store/<string:name>/item')
+def get_item_in_store(name):
+    pass
 
 app.run(port=3030)
 
