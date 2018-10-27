862ddebc0db43991ed7b07660124b25b70d50f0a
Pablo.Fuentes <pablo.fuentes@intelygez.com>
2018-10-27 15:28:01 +0200

    get and post working with their http codes propertly

--- app.py
+++ app.py
@@ -8,8 +8,15 @@ api= Api(app)
 items =[]
 class Item(Resource):
     def get(self, name):
-        return {'item': name}
+        for item in items:
+            if item['name'] == name:
+                return item
+            return {'item': None}, 404
 
+    def post(self, name):
+        item = {'name': name, 'price': 15.00}
+        items.append(item)
+        return item, 201
 api.add_resource(Item, '/item/<string:name>')
 
 app.run(port=3030)
