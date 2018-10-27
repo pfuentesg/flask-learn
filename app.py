7f315c14e1de2718c2b8abc942c8a1cedd44257b
Pablo.Fuentes <pablo.fuentes@intelygez.com>
2018-10-27 17:04:28 +0200

    add put and parser

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 from security import authenticate, identity
 
@@ -12,12 +12,34 @@ jwt = JWT(app, authenticate, identity)
 
 items =[]
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+    type=float,
+    required=True,
+    help="This field cannot be left blank!"
+)
     @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name']==name, items), None)
         return {'item': item}, 200 if item else 404
 
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'Item deleted'}
 
+    def put(self, name):
+        data = Item.parser.parse_args()
+        # Once again, print something not in the args to verify everything works
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
+
+    
 class Items(Resource):
     def get(self):
         return {'items:': items}
