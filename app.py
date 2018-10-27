3fee3457c77eef8a1c6ade151187d1b4a071cab5
Pablo.Fuentes <pablo.fuentes@intelygez.com>
2018-10-27 16:51:34 +0200

    include authentication with jwt, user class and security methods

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+from security import authenticate, identity
 
 app = Flask(__name__)
-
+app.secret_key = 'paco'
 api= Api(app)
+
+
+jwt = JWT(app, authenticate, identity)
 
 items =[]
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return item
-            return {'item': None}, 404
+        item = next(filter(lambda x: x['name']==name, items), None)
+        return {'item': item}, 200 if item else 404
 
 
 class Items(Resource):
@@ -20,6 +24,8 @@ class Items(Resource):
 
     def post(self):
         data = request.get_json()
+        if  next(filter(lambda x: x['name']==data['name'], items), None):
+            return{'message': 'item already exists'}, 400
         item = {'name': data['name'], 'price': data['price']}
         items.append(item)
         return item, 201
