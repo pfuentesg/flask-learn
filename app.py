2e30e5ea5feac9ba4bbf66379ef391e3c650282e
Pablo.Fuentes <pablo.fuentes@intelygez.com>
2018-10-28 18:04:25 +0100

    add parser to post and jwt decorator to all methods

--- app.py
+++ app.py
@@ -23,11 +23,13 @@ class Item(Resource):
         item = next(filter(lambda x: x['name']==name, items), None)
         return {'item': item}, 200 if item else 404
 
+    @jwt_required()
     def delete(self, name):
         global items
         items = list(filter(lambda x: x['name'] != name, items))
         return {'message': 'Item deleted'}
 
+    @jwt_required()
     def put(self, name):
         data = Item.parser.parse_args()
         # Once again, print something not in the args to verify everything works
@@ -41,11 +43,23 @@ class Item(Resource):
 
     
 class Items(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+    type=float,
+    required=True,
+    help="This field cannot be left blank!"
+)
+    parser.add_argument('name',
+    required=True,
+    help="This field cannot be left blank!"
+)
+    @jwt_required()
     def get(self):
         return {'items:': items}
 
+    @jwt_required()
     def post(self):
-        data = request.get_json()
+        data = Items.parser.parse_args()
         if  next(filter(lambda x: x['name']==data['name'], items), None):
             return{'message': 'item already exists'}, 400
         item = {'name': data['name'], 'price': data['price']}
