f709ccbc82b8c5ddae7d4547c4533ae3fcb07d1d
Pablo.Fuentes <pablo.fuentes@intelygez.com>
2018-10-28 20:18:04 +0100

    refactor second app to app, remove items and finish register method

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 from security import authenticate, identity
-
+from user import UserRegister
 app = Flask(__name__)
 app.secret_key = 'paco'
 api= Api(app)
@@ -65,7 +65,7 @@ class Items(Resource):
         item = {'name': data['name'], 'price': data['price']}
         items.append(item)
         return item, 201
-
+api.add_resource(UserRegister, '/register')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(Items, '/items')
 app.run(port=3030)
