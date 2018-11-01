b9a3cebfc16eacf7b913c25784295fbe5d33629c
Pablo.Fuentes <pablo.fuentes@intelygez.com>
2018-11-01 20:24:27 +0100

    update database to use ssqlAlchemy, add configuration

--- app.py
+++ app.py
@@ -12,8 +12,13 @@ api= Api(app)
 
 jwt = JWT(app, authenticate, identity)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///holi.db'
 app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=9999999999)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api.add_resource(UserRegister, '/register')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(Items, '/items')
-app.run(port=3030)
+if __name__== "__main__":
+    from db import db
+    db.init_app(app)
+    app.run(port=3030)
--- create_table.py
+++ create_table.py
@@ -7,9 +7,8 @@ cursor = connection.cursor()
 create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username text, password text)"
 cursor.execute(create_table)
 
-create_table = "CREATE TABLE IF NOT EXISTS items (name text PRIMARY KEY, price real)"
+create_table = "CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY, name text, price real)"
 cursor.execute(create_table)
-cursor.execute("INSERT INTO items VALUES ('test', 5.00)")
 connection.commit()
 
 connection.close()
