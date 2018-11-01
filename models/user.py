73d00deaf5193d14d5bf424eea56fcc5f12a4f52
Pablo.Fuentes <pablo.fuentes@intelygez.com>
2018-11-01 21:40:44 +0100

    refactor user model and resource to use SQLARCEhemy, fix typo in securyty

--- models/user.py
+++ models/user.py
@@ -1,37 +1,23 @@
 import sqlite3
+from db import db
+class UserModel(db.Model):
 
-class User(object):
-    def __init__(self, _id, username, password):
-        self.id = _id
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80))
+    password = db.Column(db.String(80))
+
+    def __init__(self, username, password):
         self.username = username
         self.password = password
 
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
     @classmethod
     def find_by_username (cls, username):
-        connection = sqlite3.connect('holi.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM users WHERE username=?"
-        result = cursor.execute(query,(username,))
-        row = result.fetchone()
-        if row:
-            user = cls(*row)
-        else:
-            user = None
-        connection.close()
-        return user
+        cls.query.filter_by(username=username).first()
 
     @classmethod
-    def find_by_id (cls, _id):
-        connection = sqlite3.connect('holi.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM users WHERE id=?"
-        result = cursor.execute(query,(_id,))
-        row = result.fetchone()
-        if row:
-            user = cls(*row)
-        else:
-            user = None
-        connection.close()
-        return user
+    def find_by_username (cls, id):
+        cls.query.filter_by(id=id).first()
--- resources/user.py
+++ resources/user.py
@@ -1,5 +1,6 @@
 import sqlite3
 from flask_restful import Resource, reqparse
+from models.user import UserModel
 class UserRegister(Resource):
 
     parser = reqparse.RequestParser()
@@ -16,16 +17,13 @@ class UserRegister(Resource):
 
     def post(self):
         data = UserRegister.parser.parse_args()
-        if User.find_by_username(data['username']):
+        print(' a buscar', data['username'])
+        holi = UserModel.find_by_username(data['username'])
+        print(holi)
+        if holi:
             return {"message": "User with that username already exists."}, 400
             
-        connection = sqlite3.connect('holi.db')
-        cursor = connection.cursor()
-
-        query = "INSERT INTO users VALUES (NULL, ?, ?)"
-        cursor.execute(query, (data['username'], data['password']))
-
-        connection.commit()
-        connection.close()
+        user = UserModel(data['username'], data['password'])
+        user.save_to_db()
 
         return {"message": "User created successfully."}, 201
